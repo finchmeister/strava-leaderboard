6b85216f0f180442b13b4ffb09283480496ffd95
Jonathan Finch <thefinchmeister@gmail.com>
2021-03-29 20:15:06 +0100

    Fix leader board ordering bug

--- generate_leaderboard.py
+++ generate_leaderboard.py
@@ -2,11 +2,17 @@ import json
 import re
 from operator import itemgetter
 from jinja2 import Template
+import datetime
+import time
 
 
 def convert_date(date_string):
     return re.findall(', (.+)', date_string)[0]
 
+
+def convert_time_to_seconds(time_string):
+    x = time.strptime(time_string, '%H:%M:%S' if len(time_string) > 5 else '%M:%S')
+    return datetime.timedelta(hours=x.tm_hour, minutes=x.tm_min, seconds=x.tm_sec).total_seconds()
 
 with open('template/index.html') as file_:
     template = Template(file_.read())
@@ -34,12 +40,13 @@ for athlete_id in athletes_data:
         sorted_athlete_data[distance].append({
             'name': athlete_data['name'],
             'time': pb['time'],
+            'timeSeconds': convert_time_to_seconds(pb['time']),
             'date': convert_date(pb['date']),
             'url': pb['url']
         })
 
 for distance in sorted_athlete_data:
-    sorted_athlete_data[distance] = sorted(sorted_athlete_data[distance], key=itemgetter('time'))
+    sorted_athlete_data[distance] = sorted(sorted_athlete_data[distance], key=itemgetter('timeSeconds'))
 
 rendered_template = template.render(sorted_athlete_data=sorted_athlete_data)
 
